9922091e4705aaf4fdf54624ce2dcfe9d4e5781c
Yevhen Liakhovych <jdominus@Yevhens-iMac.local>
2020-02-28 09:53:50 -0800

    print_list

--- src/mx_print_list.c
+++ src/mx_print_list.c
@@ -8,7 +8,10 @@ void mx_print_list (t_print arguments, t_list *files, t_flags *flags) {
             t_file *data = iter->data;
 
             if (MX_ISDIR(data->stat->st_mode)) {
+                t_list *subdir = mx_process_dir(data);
 
+                mx_sort_list(subdir, flags);
+                mx_print_list(ALL, subdir, flags);
             }
         }
     }
